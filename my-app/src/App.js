a1b340dd3149ef42cc095e790a0e9d85578d631a
Minu <122645470+Minusuke@users.noreply.github.com>
2023-06-06 14:09:19 +0000

    agregue export e import

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,2 +1,14 @@
-import Nav from './components/MyComponent';
+
+import React from 'react';
+import Template from './components/Template';
+
+const App = () => {
+  return (
+    <div>
+      <Template />
+    </div>
+  );
+};
+
+export default App;
 
